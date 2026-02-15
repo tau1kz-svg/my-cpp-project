e523b583214b9a41ecfbc76104afe7a386791289
Vitaliy Chernigin <tau1kz@gmail.com>
2026-02-15 21:26:40 +0500

    Auto-update

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <libavutil/imgutils.h>
+}
+
+#include <iostream>
 #include <windows.h>
 
 // Прототип функции обработки сообщений
