e2db223439298c0773f9973e60211c2abea0dfc6
Vitaliy Chernigin <tau1kz@gmail.com>
2026-02-15 21:01:38 +0500

    Auto-update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    setlocale(LC_ALL, "Russian");
+    setlocale(LC_ALL, "RU");
     std::cout << "Привет Друг! Ты видишь это?" << std::endl;
     return 0;
-}
+}Ъ
